bf2f8da03843706e4a8262cb5abd1b7ec04ca3bb
faema44 <faema44@gmail.com>
2026-03-22 20:35:47 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — Mercado PWA
-// Versão do cache: atualize este número para forçar atualização novo
+// Versão do cache: atualize este número para forçar atualização
 const CACHE_NAME = 'mercado-v5';
 
 // Arquivos para cache offline
